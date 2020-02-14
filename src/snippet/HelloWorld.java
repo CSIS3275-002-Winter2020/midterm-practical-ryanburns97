e55c2b6f525a36053fa42a79534c1cf2260a53c5
ryanburns97 <burnsr2@student.douglascollege.ca>
2020-02-14 13:46:47 -0800

    test

---
+++ src/snippet/HelloWorld.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Ryan Burns");
+		System.out.println("burnsr2@student.douglascollege.ca");
+		
+		
+	}
+
+}
